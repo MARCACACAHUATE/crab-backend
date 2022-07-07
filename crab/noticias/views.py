57c3674a291d9004540170ebd4729e75687c7111
papumc <marcacacahuate@outlook.com>
2022-07-07 00:17:15 -0500

    noticias: Endpoint para listar noticias utilizando queryparams para filtrar por fechas

--- crab/noticias/views.py
+++ crab/noticias/views.py
@@ -1,19 +1,39 @@
+from datetime import date
+
 from rest_framework import mixins, viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser, AllowAny
 
-from noticias.serializers import ListNoticiaSerializer
+from noticias.serializers import ListNoticiaSerializer, NoticiaModelSerializer
 from .models import Noticia
 
 
-class NoticiaViewSet(mixins.DestroyModelMixin, viewsets.GenericViewSet):
+class NoticiaViewSet(mixins.ListModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = Noticia.objects.all()
-    serializer_class = ListNoticiaSerializer
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return ListNoticiaSerializer
+        return NoticiaModelSerializer
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
         context["categorias"] = []
         return context
+
+    def get_queryset(self):
+        queryset = Noticia.objects.all()
+        fecha_fin = self.request.query_params.get("fecha_fin")
+        fecha = self.request.query_params.get("fecha")
+
+        if fecha_fin and fecha:
+            queryset = queryset.filter(fecha__range=(fecha, fecha_fin))
+        elif fecha_fin or fecha:
+            fecha = fecha_fin or fecha
+            queryset = queryset.filter(fecha=fecha)
+        else: 
+            queryset = queryset.filter(fecha=date.today().isoformat())
+        return queryset
 
     def get_permissions(self):
         if self.action == "destroy":
