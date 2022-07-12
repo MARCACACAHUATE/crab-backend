a126a2a85de6eb36bbcd133d151afe1120061e81
papumc <marcacacahuate@outlook.com>
2022-07-12 18:47:50 -0500

    datasets: el creador de un dataset lo puede eliminar

--- crab/datasets/views.py
+++ crab/datasets/views.py
@@ -1,17 +1,20 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, mixins
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-from .serializers import DatasetSerializer, CreateDatasetSerializer
+from .serializers import CreateDatasetSerializer
 from .models import Dataset
+from .permissions import IsDatasetOwner
 
-class DatasetViewSet(viewsets.GenericViewSet):
+class DatasetViewSet(mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = Dataset.objects.all()
     serializer_class = CreateDatasetSerializer
 
     def get_permissions(self):
         if self.action in ["create"]:
             permissions = [IsAuthenticated]
+        elif self.action in ["destroy"]:
+            permissions = [IsDatasetOwner, IsAuthenticated]
         else:
             permissions = [AllowAny]
         return [permission() for permission in permissions]
