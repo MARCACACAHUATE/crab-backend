117bbdb53b01c09b6d6f9e5bde4c4c6850c2f02e
papumc <marcacacahuate@outlook.com>
2022-07-12 20:20:09 -0500

    datasets: endpoint que lista los dataset creados por el usuario que esta haciendo el request

--- crab/datasets/views.py
+++ crab/datasets/views.py
@@ -2,22 +2,30 @@ from rest_framework import viewsets, status, mixins
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-from .serializers import CreateDatasetSerializer
+from .serializers import DatasetSerializer, CreateDatasetSerializer
 from .models import Dataset
 from .permissions import IsDatasetOwner
 
-class DatasetViewSet(mixins.DestroyModelMixin, viewsets.GenericViewSet):
-    queryset = Dataset.objects.all()
-    serializer_class = CreateDatasetSerializer
+class DatasetViewSet(mixins.ListModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return DatasetSerializer
+        return CreateDatasetSerializer
 
     def get_permissions(self):
         if self.action in ["create"]:
             permissions = [IsAuthenticated]
-        elif self.action in ["destroy"]:
+        elif self.action in ["destroy", "update", "partial_update"]:
             permissions = [IsDatasetOwner, IsAuthenticated]
         else:
             permissions = [AllowAny]
         return [permission() for permission in permissions]
+
+    def get_queryset(self):
+        owner = self.request.user
+        queryset = Dataset.objects.filter(user=owner)
+        return queryset
 
     def create(self, request):
         serializer = self.get_serializer(data=request.data)
