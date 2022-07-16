b2062915f11c903e874a12459bf36106441fec77
papumc <marcacacahuate@outlook.com>
2022-07-15 20:53:17 -0500

    soporte para variables de entorno con django-environ

--- crab/crab/settings.py
+++ crab/crab/settings.py
@@ -12,19 +12,23 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# env config
+env = environ.Env()
+environ.Env.read_env(Path(BASE_DIR.parent, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^g9!fc2)$o#0+82c)+m3m+p*znituh^=36i4tm^2qs0@^(#z3+'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG", bool, default=False)
 
 ALLOWED_HOSTS = []
 
